ad127ce5ec1b6785e5648e7c82ef1c0440b06b3c
krishna <krihna.221ee130@nitk.edu.in>
2024-04-12 20:57:57 +0530

    home_about_fend_added

--- smile_backend/blogs/urls.py
+++ smile_backend/blogs/urls.py
@@ -1,12 +1,19 @@
 # urls.py
 
 from django.urls import path
-from .views import PostDetailView, add_comment_to_post
-from .views import team_list_view
+from .views import PostDetailView, add_comment_to_post,team_list_view
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('post/<uuid:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('teams/', team_list_view, name='team-list'),
     path('post/<uuid:pk>/comment/', add_comment_to_post, name='add-comment'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='home'),
     
     # Other URL patterns as needed
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- smile_backend/smile_backend/urls.py
+++ smile_backend/smile_backend/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogs/', include('blogs.urls')),
+    path('', include('blogs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
